427f958137b5cec6aa88d73bd3f5a4bf4c3488ae
Vincent <vincent.rochas@epitech.eu>
2019-09-25 11:06:55 +0200

    fix clock

---
+++ api/priv/repo/migrations/3_create_manage.exs
@@ -0,0 +1,12 @@
+defmodule App.Repo.Migrations.Manage do
+  use Ecto.Migration
+
+  def change do
+    create table(:manage) do
+      add :user_id, references(:users)
+      add :team_id, references(:teams)
+    end
+
+    create unique_index(:manage, [:user_id, :team_id])
+  end
+end
